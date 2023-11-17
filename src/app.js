42fea4b7fcb7aa6e28580fede99118ed1d4bd573
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-17 12:22:09 +0100

    Making the code prettier

--- src/app.js
+++ src/app.js
@@ -1,40 +1,39 @@
 export class Main {
-    greet(name){
-
-        if(this.isNullUnedinedOrEmpty(name)){
-            return 'Hello, my friend.';
-        }
-
-        if(this.lengthEqualsToTwo(name)) {
-            return 'Hello, ' + name[0] + ' and ' + name[1] + '.'; 
-        }
-
-        if(this.lengthSuperiorThanTwo(name)) { 
-            let toutSaufDernier = name.slice(0, name.length-1).join(', ');
-            let dernier = name.slice(name.length-1);
-            return `Hello, ${toutSaufDernier} and ${dernier}.`; 
-        }
-         
-        if(this.upperCase(name)){
-            return 'HELLO, ' + name + '!';
-         }
-
-     return 'Hello, ' + name + '.';
+  greet(name) {
+    if (this.isNullUndefinedOrEmpty(name)) {
+      return "Hello, my friend.";
     }
 
-    isNullUnedinedOrEmpty(name){
-        return name === null || name===undefined || name === '';
+    if (this.lengthEqualsToTwo(name)) {
+      return "Hello, " + name[0] + " and " + name[1] + ".";
     }
 
-    upperCase(name){
-        return name===name.toUpperCase();
-     }
+    if (this.lengthSuperiorThanTwo(name)) {
+      let toutSaufDernier = name.slice(0, name.length - 1).join(", ");
+      let dernier = name.slice(name.length - 1);
+      return `Hello, ${toutSaufDernier} and ${dernier}.`;
+    }
 
-     lengthEqualsToTwo(name){
-        return Array.isArray(name) && name.length===2;
-     }
+    if (this.upperCase(name)) {
+      return "HELLO, " + name + "!";
+    }
 
-     lengthSuperiorThanTwo(name) {
-        return Array.isArray(name) && name.length >= 3;
-     }
+    return "Hello, " + name + ".";
+  }
+
+  isNullUndefinedOrEmpty(name) {
+    return name === null || name === undefined || name === "";
+  }
+
+  upperCase(name) {
+    return name === name.toUpperCase();
+  }
+
+  lengthEqualsToTwo(name) {
+    return Array.isArray(name) && name.length === 2;
+  }
+
+  lengthSuperiorThanTwo(name) {
+    return Array.isArray(name) && name.length >= 3;
+  }
 }
