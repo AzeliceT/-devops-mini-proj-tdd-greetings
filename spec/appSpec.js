2888c1ca83bd38d765da1db62f12b0c15421cc22
ana.araujo <ana.araujo@student.vinci.be>
2023-11-17 01:05:00 +0100

    Question1 Refractoring

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -4,9 +4,10 @@ describe("A suite of tests for the greet function of the main class", function()
 
     it("greet function should return 'Hello, Bob.' when given 'Bob'", function(){
 
+        let name = "Bob";
         let main = new Main();
-        let result = main.greet("Bob");
+        let result = main.greet(name);
 
-        expect(result).toBe("Hello, Bob.");
+        expect(result).toBe(`Hello, ${name}.`);
     });
 });
