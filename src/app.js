eb76be6698f86e988a24671477530dbe97efdc2a
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-17 19:51:47 +0100

    Refactoring Question7

--- src/app.js
+++ src/app.js
@@ -5,57 +5,59 @@ export class Main {
     }
 
     if (this.lengthEqualsToTwo(name)) {
-        if(name[name.length-1] === 'fr'){
-            return 'Bonjour, ' + name[0] + '.';
-        }
+      if (this.isInFrench(name)) {
+        return "Bonjour, " + name[0] + ".";
+      }
 
-        if(name[name.length-1] === 'nl'){
-            return 'Dag, ' + name[0] + '.';
-        }
+      if (this.isInDutch(name)) {
+        return "Dag, " + name[0] + ".";
+      }
 
-        if(name[name.length-1] === 'en'){
-            return 'Hello, ' + name[0] + '.';
-        }
+      if (this.isInEnglish(name)) {
+        return "Hello, " + name[0] + ".";
+      }
 
-        return "Hello, " + name[0] + " and " + name[1] + ".";
+      return "Hello, " + name[0] + " and " + name[1] + ".";
     }
 
-    if(this.lengthSuperiorThanTwo(name)) {
-        if(name[name.length-1] === 'fr'){
-            return 'Bonjour, ' + name[0] + " et " + name[1] + '.';
-         }
+    if (this.lengthSuperiorThanTwo(name)) {
+      if (this.isInFrench(name)) {
+        return "Bonjour, " + name[0] + " et " + name[1] + ".";
+      }
 
-         if(name[name.length-1] === 'nl'){
-            return 'Dag, ' + name[0] + " en " + name[1] + '.';
-         }
+      if (this.isInDutch(name)) {
+        return "Dag, " + name[0] + " en " + name[1] + ".";
+      }
 
-         if(name[name.length-1] === 'en'){
-            return 'Hello, ' + name[0] + " and " + name[1] + '.';
-         }
+      if (this.isInEnglish(name)) {
+        return "Hello, " + name[0] + " and " + name[1] + ".";
+      }
 
-        const lowerNames = [];
-        const upperNames = [];
-        let message = "Hello, ";
+      const lowerNames = [];
+      const upperNames = [];
+      let message = "Hello, ";
 
-        for (const nom of name) {
-           if((nom === nom.toUpperCase())){
-              upperNames.push(nom);
-           } else {
-              lowerNames.push(nom);
-
-           }
+      for (const nom of name) {
+        if (this.upperCase(nom)) {
+          upperNames.push(nom);
+        } else {
+          lowerNames.push(nom);
         }
+      }
 
-        message += lowerNames.slice(0,name.length-1).join(' and ') + lowerNames.slice(name.length-1) + ". ";
-        message += 'AND HELLO ' + upperNames[0] + " !";
+      message +=
+        lowerNames.slice(0, name.length - 1).join(" and ") +
+        lowerNames.slice(name.length - 1) +
+        ". ";
+      message += "AND HELLO " + upperNames[0] + " !";
 
-        if(upperNames.length > 0){
-           return message;
-        }
+      if (upperNames.length > 0) {
+        return message;
+      }
 
-        let toutSaufDernier = name.slice(0, name.length-1).join(', ');
-        let dernier = name.slice(name.length-1);
-        return `Hello, ${toutSaufDernier} and ${dernier}.`; 
+      let toutSaufDernier = name.slice(0, name.length - 1).join(", ");
+      let dernier = name.slice(name.length - 1);
+      return `Hello, ${toutSaufDernier} and ${dernier}.`;
     }
 
     if (this.upperCase(name)) {
@@ -79,5 +81,17 @@ export class Main {
 
   lengthSuperiorThanTwo(name) {
     return Array.isArray(name) && name.length >= 3;
+  }
+
+  isInFrench(name) {
+    return name[name.length - 1] === "fr";
+  }
+
+  isInDutch(name) {
+    return name[name.length - 1] === "nl";
+  }
+
+  isInEnglish(name) {
+    return name[name.length - 1] === "en";
   }
 }
